ba4c7fcd09d2890e72b9db5d0239397580aa58e6
Vlad-Marian MARAIN <transilvlad@gmail.com>
2025-11-23 13:32:16 +0100

    Remove redundant result from LocalStorageClientMock

--- src/test/java/com/mimecast/robin/storage/LocalStorageClientMock.java
+++ src/test/java/com/mimecast/robin/storage/LocalStorageClientMock.java
@@ -1,17 +1,13 @@
 package com.mimecast.robin.storage;
 
 import com.mimecast.robin.config.server.ServerConfig;
-import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * Local storage client mock for testing.
  */
-class LocalStorageClientMock extends LocalStorageClient {
+public class LocalStorageClientMock extends LocalStorageClient {
 
-    private final Pair<Integer, String> result;
-
-    public LocalStorageClientMock(ServerConfig config, Pair<Integer, String> result) {
+    public LocalStorageClientMock(ServerConfig config) {
         this.config = config;
-        this.result = result;
     }
 }
